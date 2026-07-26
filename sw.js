36b690a6910a298b09655f2fc26d81cb95bede65
Roshan Toppo <toppointernetcafe2021@gmail.com>
2026-07-26 15:42:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,40 @@
 const CACHE_NAME = 'kurukh-converter-v1';
+
 const urlsToCache = [
   '/Hindi-Devnagri---Kurukh-converter/',
   '/Hindi-Devnagri---Kurukh-converter/index.html',
+  '/Hindi-Devnagri---Kurukh-converter/manifest.json',
   '/Hindi-Devnagri---Kurukh-converter/kurukh.ttf',
-  '/Hindi-Devnagri---Kurukh-converter/icon-192.png',
-  '/Hindi-Devnagri---Kurukh-converter/icon-512.png'
+  '/Hindi-Devnagri---Kurukh-converter/192.png',
+  '/Hindi-Devnagri---Kurukh-converter/512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
 
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(name => {
-          if (!cacheWhitelist.includes(name)) return caches.delete(name);
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-      )
-    )
+      );
+    })
   );
-);
+});
